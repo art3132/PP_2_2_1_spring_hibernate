0a82ed5724d1dd7d48289ba1a82b875dbd54b3b3
Артём <art3132@gmail.com>
2023-01-30 13:33:33 +0300

    Deleted @Autowired

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/CarServiceImpl.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/CarServiceImpl.java
@@ -2,7 +2,6 @@ package hiber.service;
 
 import hiber.dao.CarDao;
 import hiber.model.Car;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,8 +9,12 @@ import java.util.List;
 
 @Service
 public class CarServiceImpl implements CarService {
-    @Autowired
-    private CarDao carDao;
+    private final CarDao carDao;
+
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
     @Transactional
     @Override
     public void add (Car car) {
